842615532d6808c93784c6bd2b7b1bb2d062fe1a
JGJ <jordan.gb0@gmail.com>
2023-06-11 15:49:21 -0400

    started SetRep container

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import Button from 'react-bootstrap/Button'
+import'../src/index.css';
+import{Container} from "@material-ul/core"
+
+function Header() {
   return (
-    <div className="App">
-   
+    <div>
+<Container style={{backgroundColor:"skyblue"}}>
+    <header className="header">
+      <h1 className='h1'>SetRep</h1>
+    </header>
+    </Container>
     </div>
   );
 }
 
-export default App;
+export default Header;
