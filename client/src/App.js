6d22ca26251f82b1b7b09a0ac8e4a87dece666fe
JGJ <jordan.gb0@gmail.com>
2023-06-18 21:52:12 -0400

    added code to allow me to change background color

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
-import Button from 'react-bootstrap/Button'
 import'../src/index.css';
-import{Container} from "@material-ul/core"
 
 function Header() {
   return (
-    <div>
-<Container style={{backgroundColor:"skyblue"}}>
+    <div className='container'>
+    
     <header className="header">
       <h1 className='h1'>SetRep</h1>
     </header>
-    </Container>
     </div>
   );
 }
