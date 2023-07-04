746195df19087c1d4601fa25510c1d34f899a2b7
JGJ <jordan.gb0@gmail.com>
2023-07-04 16:38:20 -0400

    Added text box to enter numbers, start button

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import'../src/index.css';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
 
 function Header() {
   return (
@@ -6,7 +8,15 @@ function Header() {
     
     <header className="header">
       <h1 className='h1'>SetRep</h1>
+      <h1 className='SetsQuestion'> How many sets?</h1>
     </header>
+    <Form className='inputBox'>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Control type="email" placeholder="Number between 1 and 15" />
+      </Form.Group>
+    </Form>
+    <Button className='GoButton'>Here is the button
+    </Button>
     </div>
   );
 }
