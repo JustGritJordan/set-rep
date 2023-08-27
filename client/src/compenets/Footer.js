629349edbac233d2b35fa5b5fb6f9f65edbc6bf3
JGJ <jordan.gb0@gmail.com>
2023-08-27 17:37:24 -0400

    contining work on how to add background color of footer, might have createed it not in the ebst way.

--- client/src/compenets/Footer.js
+++ client/src/compenets/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Container, Row, Col} from 'react-bootstrap'
 
 const Footer = () => {
-  return <footer>
+  return <footer bg="dark" variant="dark">
     {/* <Container> */}
-        <Row>
-           <Col className='text-center py-3'>Copyright &copy; ProShop</Col> 
+        <Row >
+           <Col className='text-center py-3' >Copyright &copy; ProShop</Col> 
         </Row>
     {/* </Container> */}
   </footer> 
