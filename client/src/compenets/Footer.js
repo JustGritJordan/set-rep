b845dc2c4b06eddeb09278dbc4e7063e1ef49fc5
JGJ <jordan.gb0@gmail.com>
2023-08-28 22:49:43 -0400

    added first code to footer

--- client/src/compenets/Footer.js
+++ client/src/compenets/Footer.js
@@ -4,6 +4,21 @@ import { Container, Row, Col} from 'react-bootstrap'
 const Footer = () => {
   return <footer bg="dark" variant="dark">
     {/* <Container> */}
+    <div className = "main-footer">
+    {/* <div className ="container"> */}
+    <div className ="row">
+    <div className='col-md-3 col-sm-6'>
+    <h4> Title</h4>
+    <ul>
+        <li> What it do</li>
+        <li> Howdy Partner</li>
+        <li> I like muscle butt anime girls lol </li>
+    </ul>
+
+    </div>
+    </div> 
+    {/* </div> */}
+    </div>
         <Row >
            <Col className='text-center py-3' >Copyright &copy; ProShop</Col> 
         </Row>
